1c41d15eee5b60bc91b7dda70ae5c0835aaeb446
ShaneMarusczak <marusczaks@gmail.com>
2022-06-02 17:25:56 -0400

    vm with stack

--- src/main.c
+++ src/main.c
@@ -1,23 +1,37 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char *argv[])
 {
+    initVM();
+
     Chunk chunk;
     initChunk(&chunk);
 
-    int constant = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT, 1);
-    writeChunk(&chunk, constant, 1);
+    int constant = addConstant(&chunk, 2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    int constant2 = addConstant(&chunk, 1.3);
-    writeChunk(&chunk, OP_CONSTANT, 1);
-    writeChunk(&chunk, constant2, 1);
+    constant = addConstant(&chunk, 6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
 
-    writeChunk(&chunk, OP_RETURN, 2);
+    writeChunk(&chunk, OP_ADD, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    constant = addConstant(&chunk, 4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constant, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+    writeChunk(&chunk, OP_NEGATE, 123);
+
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    // disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    freeVM();
     freeChunk(&chunk);
     return 0;
 }
